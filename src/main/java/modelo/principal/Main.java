f23fa79df99f9c258a63f2ed4ffbdd69fcdfb93e
MIGUEL VIEIRO DA SILVA <miguel_v_silva@estudante.sesisenai.org.br>
2024-11-13 10:31:52 -0300

    Feito a classe de testes

--- src/main/java/modelo/principal/Main.java
+++ src/main/java/modelo/principal/Main.java
@@ -2,23 +2,21 @@ package modelo.principal;
 
 
 import modelo.dao.desenvolvedor.DesenvolvedorDAOImpl;
-import modelo.dao.usuario.UsuarioDAOImpl;
 import modelo.entidade.desenvolvedor.Desenvolvedor;
-import modelo.entidade.usuario.Usuario;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		UsuarioDAOImpl usuarioDAO = new UsuarioDAOImpl();
-		
 		DesenvolvedorDAOImpl desenvolvedorDAO = new DesenvolvedorDAOImpl();
 		
-		for (Usuario usuario : usuarioDAO.recuperarUsuarios()) {
-			System.out.println("bloqueado: "+usuario.isBloqueado());
+        Desenvolvedor desenvolvedor = desenvolvedorDAO.recuperarDesenvolvedorPorIdUsaurio(1L);
+		
+		if (desenvolvedor==null) {
+			System.out.println("aaa");
+		}else {
+			System.out.println("bbb");
 		}
-		
-		
-		
 	}
 
 }
